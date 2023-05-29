5d39e656a258f3360cf96818a9e66be2e702f339
alwinvankuijk <alwinvankuijk@gmail.com>
2023-05-29 16:51:27 +0200

    Created a build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ecommerce-product-page/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
